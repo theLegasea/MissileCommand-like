7f92156c8147859674ca2d088e1052cc5a32583f
theLegasea <148804473+theLegasea@users.noreply.github.com>
2025-02-19 17:38:29 -0500

    savepoint in case of oops

---
+++ GameOver.py
@@ -0,0 +1,52 @@
+﻿import pygame
+import Cursor
+import GuiStuff
+import InGame
+
+
+# print game over to middle of screen
+# print your score
+# print a play again button
+def game_over(gui):
+    pygame.init()
+    height = 480
+    width = 640
+    screen = pygame.display.set_mode((640, 480))
+    clock = pygame.time.Clock()
+    running = True
+    dt = 0
+
+
+    cursor = Cursor.Cursor(screen)
+
+
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill("tomato")
+
+        # all ogre now
+        font = pygame.font.SysFont(None, 36)
+        gameOver = font.render("Game Over", True, "black")
+        screen.blit(gameOver, (screen.get_width() / 2 - gameOver.get_width() / 2, screen.get_height() / 2 - gameOver.get_height() / 2 - 50))
+        # your score
+        yourScore = font.render("Your Score:" + str(gui.score), True, "black")
+        screen.blit(yourScore, (screen.get_width() / 2 - yourScore.get_width() / 2, screen.get_height() / 2 - yourScore.get_height() / 2))
+        # again
+        playAgain = pygame.Rect(screen.get_width() / 2 - 75, screen.get_height() / 2 + 50, 150, 50)
+        pygame.draw.rect(screen, "green", playAgain)
+        playText = font.render("Play Again", True, "black")
+        screen.blit(playText, (playAgain.x + playAgain.width / 2 - playText.get_width() / 2, playAgain.y + playAgain.height / 2 - playText.get_height() / 2))
+
+        if playAgain.collidepoint(cursor.x, cursor.y) and event.type == pygame.MOUSEBUTTONDOWN:
+            InGame.playgame()
+
+        cursor.draw()
+        cursor.update()
+        pygame.display.flip()
+        clock.tick(60) / 1000
+
+    pygame.quit()
--- main.py
+++ main.py
@@ -1,50 +1,55 @@
 ﻿import pygame
 import Cursor
+import InGame
+import asyncio
 
 # 640 by 480 reso
 # 3 bases on the ground - QWE shoots
 # player moves cursor with keypad?
 
+async def main():
+    pygame.init()
+    height = 480
+    width = 640
+    screen = pygame.display.set_mode((640, 480))
+    clock = pygame.time.Clock()
+    running = True
+    dt = 0
 
-pygame.init()
-height = 480
-width = 640
-screen = pygame.display.set_mode((640, 480))
-clock = pygame.time.Clock()
-running = True
-dt = 0
-
-# Cursor
-cursor = Cursor.Cursor(screen)
-
-
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    # Cursor
+    cursor = Cursor.Cursor(screen)
 
 
-    # render the gray backdrop
-    screen.fill("lightgray")
-    # print the title "Almost Missile Command" at the top of the screen
-    font = pygame.font.SysFont(None, 36)
-    title = font.render("Almost Missile Command", True, "black")
-    screen.blit(title, (screen.get_width() / 2 - title.get_width() / 2, 10))
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-    # Play button in the center of the screen
-    playButton = pygame.Rect(screen.get_width() / 2 - 50, screen.get_height() / 2 - 25, 100, 50)
-    pygame.draw.rect(screen, "red", playButton)
-    playText = font.render("Play", True, "black")
-    screen.blit(playText, (playButton.x + playButton.width / 2 - playText.get_width() / 2, playButton.y + playButton.height / 2 - playText.get_height() / 2))
 
-    cursor.update()
+        # render the gray backdrop
+        screen.fill("lightgray")
+        # print the title "Almost Missile Command" at the top of the screen
+        font = pygame.font.SysFont(None, 36)
+        title = font.render("Almost Missile Command", True, "black")
+        screen.blit(title, (screen.get_width() / 2 - title.get_width() / 2, 10))
 
-    if playButton.collidepoint(cursor.x, cursor.y) and event.type == pygame.MOUSEBUTTONDOWN:
-        import InGame
+        # Play button in the center of the screen
+        playButton = pygame.Rect(screen.get_width() / 2 - 50, screen.get_height() / 2 - 25, 100, 50)
+        pygame.draw.rect(screen, "red", playButton)
+        playText = font.render("Play", True, "black")
+        screen.blit(playText, (playButton.x + playButton.width / 2 - playText.get_width() / 2, playButton.y + playButton.height / 2 - playText.get_height() / 2))
 
 
 
-    cursor.draw()
+        cursor.update()
 
-    pygame.display.flip()
-pygame.quit()
+        if playButton.collidepoint(cursor.x, cursor.y) and event.type == pygame.MOUSEBUTTONDOWN:
+            InGame.playgame()
+
+
+
+        cursor.draw()
+
+        pygame.display.flip()
+    pygame.quit()
+asyncio.run(main())
